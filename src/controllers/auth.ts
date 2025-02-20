55ed671019d079dafef3d325ac3fde9be6f596c0
ChhunlinOn <chhunlin.on@institute.pse.ngo>
2025-02-20 15:36:56 +0700

    doing on coupon

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,11 +1,16 @@
 import { comparePassword, encryptPassword, generateToken } from '../utils/encrypt';
-import { Request, Response } from "express";
+import { Request, response, Response } from "express";
 import { UserInfo } from "../entity/user.entity";
 import { AppDataSource } from '../config';
 import { RoleEnum, RoleType } from '../common';
+import { Coupon_User } from '../entity/coupon_user.entity';
+import { generateCode } from '../utils/generate_code';
+import { Coupon } from '../entity/coupon.entity';
 
 export const register = async (req: Request, res: Response) => {
   const userRepo = AppDataSource.getRepository(UserInfo);
+  const couponuserRepo = AppDataSource.getRepository(Coupon_User);
+  const couponRepo = AppDataSource.getTreeRepository(Coupon)
   const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
@@ -20,6 +25,9 @@ export const register = async (req: Request, res: Response) => {
       message: "user already exist!",
     });
   }
+  const id = '19ef43aa-bdc3-4686-a169-0806da5562e9';
+  const couponRespone = await couponRepo.findOne({ where: { id: id } });
+ 
 
   const hashPassword = await encryptPassword(password);
   const user = new UserInfo();
@@ -27,11 +35,21 @@ export const register = async (req: Request, res: Response) => {
   user.userEmail = email;
   user.password = hashPassword;
 
-  await userRepo.save(user);
+  const userRespone = await userRepo.save(user);
 
   const token = generateToken({ id: user.id, role: RoleEnum[2] });
 
-  return res
+  const couponuser = new Coupon_User();
+  couponuser.coupon_code = `GYM-${generateCode(10)}`;
+  couponuser.user = userRespone;
+  couponuser.used = 0;
+  if (couponRespone) {
+    couponuser.coupon = couponRespone;
+  } else {
+    return res.status(500).json({ message: "Coupon not found" });
+  }
+  const couponuserRespone =  await couponuserRepo.save(couponuser);
+    return res
     .status(200)
     .json({ message: "User created successfully" });
 };
